17f30618de333cceea3b8422ad5e09aa8e6b3766
FredericDT <frederic.dt.twh@gmail.com>
2020-01-22 18:05:34 +0800

    Support 1.15.2 spigot-api

--- src/main/java/me/josvth/randomspawn/RandomSpawn.java
+++ src/main/java/me/josvth/randomspawn/RandomSpawn.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
 import org.bukkit.block.Block;
@@ -17,194 +19,203 @@ import me.josvth.randomspawn.handlers.CommandHandler;
 import me.josvth.randomspawn.handlers.YamlHandler;
 import me.josvth.randomspawn.listeners.*;
 
-public class RandomSpawn extends JavaPlugin{
+public class RandomSpawn extends JavaPlugin {
 
-	public YamlHandler yamlHandler;
-	CommandHandler commandHandler;
+    public YamlHandler yamlHandler;
+    CommandHandler commandHandler;
 
-	RespawnListener respawnListener;
-	JoinListener joinListener;
-	WorldChangeListener worldChangeListener;
-	SignListener signListener;
-	DamageListener damageListener;
+    RespawnListener respawnListener;
+    JoinListener joinListener;
+    WorldChangeListener worldChangeListener;
+    SignListener signListener;
+    DamageListener damageListener;
 
-	@Override
-	public void onEnable() {
+    @Override
+    public void onEnable() {
 
-		//setup handlers
-		yamlHandler = new YamlHandler(this);
-		logDebug("Yamls loaded!");
+        //setup handlers
+        yamlHandler = new YamlHandler(this);
+        logDebug("Yamls loaded!");
 
-		commandHandler = new CommandHandler(this);
-		logDebug("Commands registered!");
+        commandHandler = new CommandHandler(this);
+        logDebug("Commands registered!");
 
-		//setup listeners
-		respawnListener = new RespawnListener(this);
-		joinListener = new JoinListener(this);
-		worldChangeListener = new WorldChangeListener(this);
-		signListener = new SignListener(this);
-		damageListener = new DamageListener(this);
+        //setup listeners
+        respawnListener = new RespawnListener(this);
+        joinListener = new JoinListener(this);
+        worldChangeListener = new WorldChangeListener(this);
+        signListener = new SignListener(this);
+        damageListener = new DamageListener(this);
 
-	}
+    }
 
-	public void logInfo(String message){
-		getLogger().info(message);
-	}
+    public void logInfo(String message) {
+        getLogger().info(message);
+    }
 
-	public void logDebug(String message){
-		if (yamlHandler.config.getBoolean("debug",false)) { getLogger().info("(DEBUG) " + message); }
-	}
+    public void logDebug(String message) {
+        if (yamlHandler.config.getBoolean("debug", false)) {
+            getLogger().info("(DEBUG) " + message);
+        }
+    }
 
-	public void logWarning(String message){
-		getLogger().warning(message);
-	}
+    public void logWarning(String message) {
+        getLogger().warning(message);
+    }
 
-	public void playerInfo(Player player, String message){
-		player.sendMessage(ChatColor.AQUA + "[RandomSpawn] " + ChatColor.RESET + message);
-	}
+    public void playerInfo(Player player, String message) {
+        player.sendMessage(ChatColor.AQUA + "[RandomSpawn] " + ChatColor.RESET + message);
+    }
 
-	// *------------------------------------------------------------------------------------------------------------*
-	// | The following chooseSpawn method contains code made by NuclearW                                            |
-	// | based on his SpawnArea plugin:                                                                             |
-	// | http://forums.bukkit.org/threads/tp-spawnarea-v0-1-spawns-targetPlayers-in-a-set-area-randomly-1060.20408/ |
-	// *------------------------------------------------------------------------------------------------------------*
+    // *------------------------------------------------------------------------------------------------------------*
+    // | The following chooseSpawn method contains code made by NuclearW                                            |
+    // | based on his SpawnArea plugin:                                                                             |
+    // | http://forums.bukkit.org/threads/tp-spawnarea-v0-1-spawns-targetPlayers-in-a-set-area-randomly-1060.20408/ |
+    // *------------------------------------------------------------------------------------------------------------*
 
-	public Location chooseSpawn(World world){
+    public Location chooseSpawn(World world) {
 
-		String worldName = world.getName();
+        String worldName = world.getName();
 
-		// I don't like this method
-		List<Integer> blacklist = new ArrayList<Integer>();
-		
-		if( yamlHandler.worlds.contains( worldName + ".spawnblacklist") )
-			blacklist = yamlHandler.worlds.getIntegerList(worldName + ".spawnblacklist");
-		else
-			blacklist = Arrays.asList(new Integer[]{8,9,10,11,18,51,81});			
-	
-		double xmin = yamlHandler.worlds.getDouble(worldName +".spawnarea.x-min", -100);
-		double xmax = yamlHandler.worlds.getDouble(worldName +".spawnarea.x-max", 100);
-		double zmin = yamlHandler.worlds.getDouble(worldName +".spawnarea.z-min", -100);
-		double zmax = yamlHandler.worlds.getDouble(worldName +".spawnarea.z-max", 100);
-				
-		// Spawn area thickness near border. If 0 spawns whole area
-		int thickness = yamlHandler.worlds.getInt(worldName +".spawnarea.thickness", 0);
+        // I don't like this method
+        List<Material> blacklist = new ArrayList<Material>();
 
-		String type = yamlHandler.worlds.getString(worldName +".spawnarea.type", "square");
-				
-		double xrand = 0;
-		double zrand = 0;
-		double y = -1;
-		
-		if(type.equalsIgnoreCase("circle")){
+        if (yamlHandler.worlds.contains(worldName + ".spawnblacklist")) {
+            blacklist = yamlHandler.worlds.getStringList(worldName + ".spawnblacklist")
+                    .parallelStream().map(Material::getMaterial).collect(Collectors.toList());
 
-			double xcenter = xmin + (xmax - xmin)/2;
-			double zcenter = zmin + (zmax - zmin)/2;
-			
-			do {
+        } else {
+            blacklist = Arrays.asList(Material.WATER, Material.LAVA, Material.OAK_LEAVES, Material.ACACIA_LEAVES,
+                    Material.BIRCH_LEAVES, Material.DARK_OAK_LEAVES, Material.JUNGLE_LEAVES, Material.SPRUCE_LEAVES, Material.FIRE, Material.CACTUS
+//                    new Integer[]{8, 9, 10, 11, 18, 51, 81}
+            );
+        }
 
-				double r = Math.random() * (xmax - xcenter);
-				double phi = Math.random() * 2 * Math.PI;
+        double xmin = yamlHandler.worlds.getDouble(worldName + ".spawnarea.x-min", -100);
+        double xmax = yamlHandler.worlds.getDouble(worldName + ".spawnarea.x-max", 100);
+        double zmin = yamlHandler.worlds.getDouble(worldName + ".spawnarea.z-min", -100);
+        double zmax = yamlHandler.worlds.getDouble(worldName + ".spawnarea.z-max", 100);
 
-				xrand = xcenter + Math.cos(phi) * r;
-				zrand = zcenter + Math.sin(phi) * r;
+        // Spawn area thickness near border. If 0 spawns whole area
+        int thickness = yamlHandler.worlds.getInt(worldName + ".spawnarea.thickness", 0);
 
-				y = getValidHighestY(world, xrand, zrand, blacklist);
-								
-			} while (y == -1);
+        String type = yamlHandler.worlds.getString(worldName + ".spawnarea.type", "square");
+
+        double xrand = 0;
+        double zrand = 0;
+        double y = -1;
+
+        if (type.equalsIgnoreCase("circle")) {
+
+            double xcenter = xmin + (xmax - xmin) / 2;
+            double zcenter = zmin + (zmax - zmin) / 2;
+
+            do {
+
+                double r = Math.random() * (xmax - xcenter);
+                double phi = Math.random() * 2 * Math.PI;
+
+                xrand = xcenter + Math.cos(phi) * r;
+                zrand = zcenter + Math.sin(phi) * r;
+
+                y = getValidHighestY(world, xrand, zrand, blacklist);
+
+            } while (y == -1);
 
 
-		} else {
+        } else {
 
-			if(thickness <= 0){
+            if (thickness <= 0) {
 
-				do {
-					
-					xrand = xmin + Math.random()*(xmax - xmin + 1);
-					zrand = zmin + Math.random()*(zmax - zmin + 1);
+                do {
 
-					y = getValidHighestY(world, xrand, zrand, blacklist);
+                    xrand = xmin + Math.random() * (xmax - xmin + 1);
+                    zrand = zmin + Math.random() * (zmax - zmin + 1);
 
-				} while (y == -1);
+                    y = getValidHighestY(world, xrand, zrand, blacklist);
 
-			}else {
+                } while (y == -1);
 
-				do {
-					
-					int side = (int) (Math.random() * 4d);
-					double borderOffset = Math.random() * (double) thickness;
-					if (side == 0) {
-						xrand = xmin + borderOffset;
-						// Also balancing probability considering thickness
-						zrand = zmin + Math.random() * (zmax - zmin + 1 - 2*thickness) + thickness;
-					}
-					else if (side == 1) {
-						xrand = xmax - borderOffset;
-						zrand = zmin + Math.random() * (zmax - zmin + 1 - 2*thickness) + thickness;
-					}
-					else if (side == 2) {
-						xrand = xmin + Math.random() * (xmax - xmin + 1);
-						zrand = zmin + borderOffset;
-					}
-					else {
-						xrand = xmin + Math.random() * (xmax - xmin + 1);
-						zrand = zmax - borderOffset;
-					}
+            } else {
 
-					y = getValidHighestY(world, xrand, zrand, blacklist);
+                do {
 
-				} while (y == -1);
-				
-			}
-		}
-	
-		Location location = new Location(world, xrand, y, zrand);
-				
-		return location;
-	}
+                    int side = (int) (Math.random() * 4d);
+                    double borderOffset = Math.random() * (double) thickness;
+                    if (side == 0) {
+                        xrand = xmin + borderOffset;
+                        // Also balancing probability considering thickness
+                        zrand = zmin + Math.random() * (zmax - zmin + 1 - 2 * thickness) + thickness;
+                    } else if (side == 1) {
+                        xrand = xmax - borderOffset;
+                        zrand = zmin + Math.random() * (zmax - zmin + 1 - 2 * thickness) + thickness;
+                    } else if (side == 2) {
+                        xrand = xmin + Math.random() * (xmax - xmin + 1);
+                        zrand = zmin + borderOffset;
+                    } else {
+                        xrand = xmin + Math.random() * (xmax - xmin + 1);
+                        zrand = zmax - borderOffset;
+                    }
 
-	private double getValidHighestY(World world, double x, double z, List<Integer> blacklist) {
-		
-		world.getChunkAt(new Location(world, x, 0, z)).load();
+                    y = getValidHighestY(world, xrand, zrand, blacklist);
 
-		double y = 0;
-		int blockid = 0;
+                } while (y == -1);
 
-		if(world.getEnvironment().equals(Environment.NETHER)){
-			int blockYid = world.getBlockTypeIdAt((int) x, (int) y, (int) z);
-			int blockY2id = world.getBlockTypeIdAt((int) x, (int) (y+1), (int) z);
-			while(y < 128 && !(blockYid == 0 && blockY2id == 0)){				
-				y++;
-				blockYid = blockY2id;
-				blockY2id = world.getBlockTypeIdAt((int) x, (int) (y+1), (int) z);
-			}
-			if(y == 127) return -1;
-		}else{
-			y = 257;
-			while(y >= 0 && blockid == 0){
-				y--;
-				blockid = world.getBlockTypeIdAt((int) x, (int) y, (int) z);
-			}
-			if(y == 0) return -1;
-		}
+            }
+        }
 
-		if (blacklist.contains(blockid)) return -1;
-		if (blacklist.contains(81) && world.getBlockTypeIdAt((int) x, (int) (y+1), (int) z) == 81) return -1; // Check for cacti
+        Location location = new Location(world, xrand, y, zrand);
 
-		return y;
-	}
+        return location;
+    }
 
-	// Methods for a save landing :)
+    private double getValidHighestY(World world, double x, double z, List<Material> blacklist) {
 
-	public void sendGround(Player player, Location location){
+        world.getChunkAt(new Location(world, x, 0, z)).load();
 
-		location.getChunk().load();
+        double y = 0;
+        Material blockType = Material.AIR;
 
-		World world = location.getWorld();
+        if (world.getEnvironment().equals(Environment.NETHER)) {
+            Material blockYType = world.getBlockAt((int) x, (int) y, (int) z).getType();
+            Material blockY2Type = world.getBlockAt((int) x, (int) (y + 1), (int) z).getType();
+            while (y < 128 && !(blockYType == Material.AIR && blockY2Type == Material.AIR)) {
+                y++;
+                blockYType = blockY2Type;
+                blockY2Type = world.getBlockAt((int) x, (int) (y + 1), (int) z).getType();
+            }
+            if (y == 127) return -1;
+        } else {
+            y = 257;
+            while (y >= 0 && blockType == Material.AIR) {
+                y--;
+                blockType = world.getBlockAt((int) x, (int) y, (int) z).getType();
+            }
+            if (y == 0) {
+                return -1;
+            }
+        }
 
-		for(int y = 0 ; y <= location.getBlockY() + 2; y++){
-			Block block = world.getBlockAt(location.getBlockX(), y, location.getBlockZ());
-			player.sendBlockChange(block.getLocation(), block.getType(), block.getData());
-		}
+        if (blacklist.contains(blockType)) return -1;
+        if (blacklist.contains(Material.CACTUS) && world.getBlockAt((int) x, (int) (y + 1), (int) z).getType() == Material.CACTUS) {
+            return -1; // Check for cacti
+        }
 
-	}
+        return y;
+    }
+
+    // Methods for a save landing :)
+
+    public void sendGround(Player player, Location location) {
+
+        location.getChunk().load();
+
+        World world = location.getWorld();
+
+        for (int y = 0; y <= location.getBlockY() + 2; y++) {
+            Block block = world.getBlockAt(location.getBlockX(), y, location.getBlockZ());
+            player.sendBlockChange(block.getLocation(), block.getType(), block.getData());
+        }
+
+    }
 }
--- src/main/java/me/josvth/randomspawn/listeners/SignListener.java
+++ src/main/java/me/josvth/randomspawn/listeners/SignListener.java
@@ -1,9 +1,6 @@
 package me.josvth.randomspawn.listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
@@ -17,69 +14,79 @@ import org.bukkit.metadata.FixedMetadataValue;
 import me.josvth.randomspawn.RandomSpawn;
 
 public class SignListener implements Listener {
-	RandomSpawn plugin;
+    RandomSpawn plugin;
 
-	public SignListener(RandomSpawn instance) {
-		plugin = instance;
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-	}
+    public SignListener(RandomSpawn instance) {
+        plugin = instance;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
 
-	@EventHandler
-	public void onPlayerSignInteract(PlayerInteractEvent event){
-		if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
-			Block block = event.getClickedBlock();
-			if (block.getTypeId() == 68 || block.getTypeId() == 63){
-				Sign sign = (Sign)block.getState();
-				final Player player = event.getPlayer();
-				if (sign.getLine(0).equalsIgnoreCase(plugin.yamlHandler.config.getString("rs-sign-text","[RandomSpawn]") ) ){
-										
-					if (player.hasPermission("RandomSpawn.usesign")){
-						
-						World world = null;
-						
-						String worldName = sign.getLine(1);
-						
-						if ( worldName != null )
-							world = Bukkit.getWorld(worldName);
-						
-						if ( world == null )
-							world = player.getWorld();
-						
-						final Location spawnLocation = plugin.chooseSpawn(world);
-						
-						plugin.sendGround(player, spawnLocation);
-						
-						player.teleport(spawnLocation.add(0, 5, 0));
-						
-						player.setMetadata("lasttimerandomspawned", new FixedMetadataValue(plugin, System.currentTimeMillis()));
-									
-						if (plugin.yamlHandler.worlds.getBoolean(world.getName() + ".keeprandomspawns",false)){
-							player.setBedSpawnLocation(spawnLocation);
-						}
+    public boolean isWallSign(Material type) {
+        return type == Material.ACACIA_WALL_SIGN || type == Material.BIRCH_WALL_SIGN || type == Material.DARK_OAK_WALL_SIGN ||
+                type == Material.JUNGLE_WALL_SIGN || type == Material.OAK_WALL_SIGN || type == Material.SPRUCE_WALL_SIGN;
+    }
 
-						if (plugin.yamlHandler.config.getString("messages.randomspawned") != null){
-							player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.yamlHandler.config.getString("messages.randomspawned")));
-						}
-												
-					}else{
-						plugin.playerInfo(player, "You don't have the permission to use this Random Spawn Sign!");
-					}
-				}
-			}
-		}
-	}
+    public boolean isStandingSign(Material type) {
+        return type == Material.ACACIA_SIGN || type == Material.BIRCH_SIGN || type == Material.DARK_OAK_SIGN ||
+                type == Material.JUNGLE_SIGN || type == Material.OAK_SIGN || type == Material.SPRUCE_SIGN;
+    }
 
-	@EventHandler
-	public void onPlayerSignPlace(SignChangeEvent event){
-		if (event.getLine(0).equalsIgnoreCase( plugin.yamlHandler.config.getString("rs-sign-text","[RandomSpawn]") ) ){
-			Player player = event.getPlayer();
-			if (player.hasPermission("RandomSpawn.placesign")){
-				this.plugin.playerInfo(player, "Random Spawn Sign created!");
-			}else{
-				event.setLine(0, "");
-				this.plugin.playerInfo(player, "You don't have the permission to place a Random Spawn Sign!");
-			}
-		}
-	}
+    @EventHandler
+    public void onPlayerSignInteract(PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block block = event.getClickedBlock();
+            if (isWallSign(block.getType()) || isStandingSign(block.getType())) {
+                Sign sign = (Sign) block.getState();
+                final Player player = event.getPlayer();
+                if (sign.getLine(0).equalsIgnoreCase(plugin.yamlHandler.config.getString("rs-sign-text", "[RandomSpawn]"))) {
+
+                    if (player.hasPermission("RandomSpawn.usesign")) {
+
+                        World world = null;
+
+                        String worldName = sign.getLine(1);
+
+                        if (worldName != null)
+                            world = Bukkit.getWorld(worldName);
+
+                        if (world == null)
+                            world = player.getWorld();
+
+                        final Location spawnLocation = plugin.chooseSpawn(world);
+
+                        plugin.sendGround(player, spawnLocation);
+
+                        player.teleport(spawnLocation.add(0, 5, 0));
+
+                        player.setMetadata("lasttimerandomspawned", new FixedMetadataValue(plugin, System.currentTimeMillis()));
+
+                        if (plugin.yamlHandler.worlds.getBoolean(world.getName() + ".keeprandomspawns", false)) {
+                            player.setBedSpawnLocation(spawnLocation);
+                        }
+
+                        if (plugin.yamlHandler.config.getString("messages.randomspawned") != null) {
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.yamlHandler.config.getString("messages.randomspawned")));
+                        }
+
+                    } else {
+                        plugin.playerInfo(player, "You don't have the permission to use this Random Spawn Sign!");
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerSignPlace(SignChangeEvent event) {
+        if (event.getLine(0).equalsIgnoreCase(plugin.yamlHandler.config.getString("rs-sign-text", "[RandomSpawn]"))) {
+            Player player = event.getPlayer();
+            if (player.hasPermission("RandomSpawn.placesign")) {
+                this.plugin.playerInfo(player, "Random Spawn Sign created!");
+            } else {
+                event.setLine(0, "");
+                this.plugin.playerInfo(player, "You don't have the permission to place a Random Spawn Sign!");
+            }
+        }
+    }
 
 }
